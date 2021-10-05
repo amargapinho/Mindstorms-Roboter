4960839178b17fc7c8b703ab903252bb856fd0ce
amargapinho <91600363+amargapinho@users.noreply.github.com>
2021-10-05 11:58:43 +0200

    Add files via upload

---
+++ Aufgabe_6.3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using MonoBrickFirmware.Display;
+using MonoBrickFirmware.Movement;
+using MonoBrickFirmware.Sensors;
+using MonoBrickFirmware.Sound;
+
+namespace Aufgabe_6._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Motor motorRechts = new Motor(MotorPort.OutC);
+            Motor motorLinks = new Motor(MotorPort.OutB);
+            EV3UltrasonicSensor ultraschallSensor = new EV3UltrasonicSensor(SensorPort.In1);
+
+            while (true)
+            {
+                int entfernung = ultraschallSensor.Read();
+                Speaker lautsprecher = new Speaker(50);
+                int tonDauer = entfernung / 150;
+
+                if (entfernung < 5)
+                {
+                    //Der Roboter bleibt stehen
+                    motorRechts.Off();
+                    motorLinks.Off();
+                    lautsprecher.Beep(0);
+                    break;
+                }
+
+                //Der Roboter fährt rückwärts & ein Piepton ist zu hören
+                else if (entfernung < 30) 
+                {
+                    Fahren(motorRechts, motorLinks, -10);
+                    lautsprecher.Beep((ushort)tonDauer);
+                }
+
+                else
+                {
+                    Fahren(motorRechts, motorLinks, 20);
+                }
+
+            }
+        }
+        private static void Fahren(Motor motorRechts, Motor motorLinks, sbyte geschwindigkeit)
+        {
+            motorRechts.SetSpeed(geschwindigkeit);
+            motorLinks.SetSpeed(geschwindigkeit);
+        }
+    }
+}
---
+++ Aufgabe_8.3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MonoBrickFirmware.Display;
+using MonoBrickFirmware.Movement;
+using MonoBrickFirmware.Sensors;
+
+namespace Aufgabe_8._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Motor motorRechts = new Motor(MotorPort.OutC);
+            Motor motorLinks = new Motor(MotorPort.OutB);
+            EV3TouchSensor beschleunigungSensor = new EV3TouchSensor(SensorPort.In4);
+            EV3TouchSensor verlangsammungSensor = new EV3TouchSensor(SensorPort.In1);
+
+            sbyte geschwindigkeit = 0;
+
+            while (true)
+            {
+                if (beschleunigungSensor.IsPressed())
+                {
+                    // In 10er Einheiten beschleunigen 
+                    Beschleunigung(motorRechts, motorLinks, geschwindigkeit);
+                }
+
+                else if (verlangsammungSensor.IsPressed())
+                {
+                    // In 10er Einheiten abbremsen 
+                    Verlangsammung(motorRechts, motorLinks, geschwindigkeit);
+                }
+
+                else
+                {
+                    motorRechts.SetSpeed(geschwindigkeit);
+                    motorLinks.SetSpeed(geschwindigkeit);
+                }
+
+                //Der Wert der Geschwindigkeit wird auf das Display angezeigt 
+                LcdConsole.WriteLine("Geschwindigkeit:" + Convert.ToString(geschwindigkeit));
+            }
+        }
+        private static void Beschleunigung(Motor motorRechts, Motor motorLinks, sbyte geschwindigkeit)
+        {
+            motorRechts.SetSpeed((sbyte)(geschwindigkeit + 10));
+            motorLinks.SetSpeed((sbyte)(geschwindigkeit + 10));
+        }
+        private static void Verlangsammung(Motor motorRechts, Motor motorLinks, sbyte geschwindigkeit)
+        {
+            motorRechts.SetSpeed((sbyte)(geschwindigkeit - 10));
+            motorLinks.SetSpeed((sbyte)(geschwindigkeit - 10));
+        }
+    }
+}
